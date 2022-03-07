8b382f38c7a16a319903a1d237888da8be4c69dd
Karana316 <kelseyrlandon@gmail.com>
2022-03-07 13:18:01 -0600

    testing

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='followthemoney',
+    version='0.0.0',
     url='https://github.com/Karana316/followthemoney',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     setup_requires=['setuptools_scm'],
     use_scm_version=True,
     install_requires=['requests', 'regex', 'pandas', 'python-dotenv'],
